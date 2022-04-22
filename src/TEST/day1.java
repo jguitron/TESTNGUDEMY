9ee2383254cd8369ebf0618ce3004ea745275e28
jguitron <jguitron@ashleyfurniture.com>
2022-04-22 09:32:37 -0400

    Initial commit

---
+++ src/TEST/day1.java
@@ -0,0 +1,17 @@
+package TEST;
+
+import org.testng.annotations.Test;
+
+public class day1 {
+    @Test
+    public void Demo()
+    {
+        System.out.println("Hello world");
+    }
+
+    @Test
+    public void SecondTest()
+    {
+        System.out.println("Bye");
+    }
+}
---
+++ src/TEST/day2.java
@@ -0,0 +1,11 @@
+package TEST;
+
+import org.testng.annotations.Test;
+
+public class day2 {
+    @Test
+    public void ploan ()
+    {
+        System.out.println("hi");
+    }
+}
